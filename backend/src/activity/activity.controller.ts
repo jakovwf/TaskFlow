160f7e02d0245c74cb0fb39777d595f8b5e511e8
jjakov19 <jovislav2002@gmail.com>
2026-06-25 12:20:28 +0200

    config files debug

--- backend/src/activity/activity.controller.ts
+++ backend/src/activity/activity.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Param, UseGuards } from '@nestjs/common';
-import { BoardMemberRole } from '@prisma/client';
+import { $Enums } from '@prisma/client';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { Roles } from '../boards/decorators/roles.decorator';
 import { BoardRoleGuard } from '../boards/guards/board-role.guard';
@@ -11,9 +11,9 @@ export class ActivityController {
   constructor(private readonly activityService: ActivityService) {}
 
   @Roles(
-    BoardMemberRole.OWNER,
-    BoardMemberRole.ADMIN,
-    BoardMemberRole.MEMBER,
+    $Enums.BoardMemberRole.OWNER,
+    $Enums.BoardMemberRole.ADMIN,
+    $Enums.BoardMemberRole.MEMBER,
   )
   @UseGuards(BoardRoleGuard)
   @Get('boards/:boardId/activity')

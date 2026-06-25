4934845a5eefc00ffc52389253f5d564ffaddf21
jjakov19 <jovislav2002@gmail.com>
2026-06-25 20:20:03 +0200

    style updated

--- frontend/src/app/features/workspace/workspace.ts
+++ frontend/src/app/features/workspace/workspace.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-workspace',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './workspace.html',
   styleUrl: './workspace.scss',
 })

b2a1cf1e19f30300a3587bf9fcbc9f218217373d
jjakov19 <jovislav2002@gmail.com>
2026-06-25 13:03:25 +0200

    styles updated

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,23 @@ module.exports = {
   },
   plugins: [require('daisyui')],
   daisyui: {
-    themes: ['light'],
+    themes: [
+      {
+        light: {
+          ...require('daisyui/src/theming/themes')['light'],
+          primary: '#2563eb',
+          secondary: '#0891b2',
+          accent: '#06b6d4',
+          neutral: '#0f172a',
+          'base-100': '#ffffff',
+          'base-200': '#f8fafc',
+          'base-300': '#e2e8f0',
+          info: '#0284c7',
+          success: '#16a34a',
+          warning: '#f59e0b',
+          error: '#dc2626',
+        },
+      },
+    ],
   },
 };
